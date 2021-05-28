19c10f6117587b9e4742d8c4d067a68fc100e92a
Inna Golovina <igigig978@gmail.com>
2021-05-28 16:16:38 +0300

    basic

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react'
+
+
+function App() {
+
+  return (
+    <div className={'container pt-3'}>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/examples/context.js
@@ -0,0 +1,5 @@
+//import React from 'react'
+//import Main from './Main'
+//import Alert from './alert/Alert'
+//import {AlertProvider} from './alert/AlertContext'
+
